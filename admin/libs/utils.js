a3a65009d84559bf7f7c2916b493fca3eaf09768
00qiu00 <h782705824@163.com>
2020-07-07 08:32:44 +0800

    六,文章功能完成

---
+++ admin/libs/utils.js
@@ -0,0 +1,16 @@
+//工具函数
+$(function (w) {
+    var utils = {
+        converToObj: function (str) {
+            var arr = str.split('&')
+            var obj = {}
+            for (var i = 0; i < arr.length; i++) {
+
+                var temp = arr[i].split('=')
+                obj[temp[0]] = temp[1]
+            }
+            return obj
+        }
+    }
+    w.utils = utils
+}(window))
